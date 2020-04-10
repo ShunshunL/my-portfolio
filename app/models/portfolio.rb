8470965716a558908cb0151dcd76cac9c2e878e8
ShunshunL <shunshunlyu@protonmail.com>
2020-04-09 20:05:41 -0700

    implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   extend FriendlyId 
   friendly_id :title, use: :slugged
 
@@ -14,7 +15,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_170726) do
+ActiveRecord::Schema.define(version: 2020_04_09_205640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_170726) do
     t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
