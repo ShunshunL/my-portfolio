849f567b9d0e3389ab6e0dd6b92aebfb33ec5e33
ShunshunL <shunshunlyu@protonmail.com>
2020-04-15 08:23:20 -0700

    added styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def login_helper 
+  def login_helper(style='nav-link')
     # Check if current_user is of class User
     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe  +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe  +
+      (link_to "Login", new_user_session_path, class: style)
     else 
       # () is here for return reason, to make sure both of these links get returned
-      link_to "Logout", destroy_user_session_path, method: :delete 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end 
   end
   
