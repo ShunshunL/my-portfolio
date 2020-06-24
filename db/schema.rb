0ef7cb87274e7f76513b0295d30b47eddb9fd807
ShunshunL <shunshunlyu@protonmail.com>
2020-06-23 18:57:30 -0700

    added attr for position in portfolio for ordering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_002431) do
+ActiveRecord::Schema.define(version: 2020_06_24_013947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_002431) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "position"
     t.index ["slug"], name: "index_portfolios_on_slug", unique: true
   end
 
