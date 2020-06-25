c24f51cb03e05b19ad7736a8723abc2e1f7e48e8
ShunshunL <shunshunlyu@protonmail.com>
2020-06-25 00:51:05 -0700

    replaced image upload buttons

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,6 @@ class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
   
-  include Placeholder
-
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   mount_uploader :thumb_image, PortfolioUploader
@@ -18,12 +16,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-  # after new is run
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
