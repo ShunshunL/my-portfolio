d9572a782813ddbdcef758dea94118237a683167
Shunshun <51725648+ShunshunL@users.noreply.github.com>
2021-03-18 20:29:02 -0700

    Update portfolio_uploader.rb

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -5,8 +5,6 @@ class PortfolioUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
